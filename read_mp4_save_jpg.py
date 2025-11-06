15319fa663bc227245e7f4f961f7afbfc9efdf32
Yu <uchen0223@gmail.com>
2025-11-06 17:19:33 +0800

    reorganize interpolation frames

--- read_mp4_save_jpg.py
+++ read_mp4_save_jpg.py
@@ -11,16 +11,18 @@ os.makedirs(output_folder, exist_ok=True)
 # 打开视频文件
 cap = cv2.VideoCapture(video_path)
 
-frame_count = 0
+loop_count, frame_count = 0, 0
 while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
+    if loop_count % 6 == 0:
+        ret, frame = cap.read()
+        if not ret or frame_count >= 100:
+            break
 
-    # 保存帧为 jpg 图片
-    save_path = os.path.join(output_folder, f"{frame_count:06d}.jpg")
-    cv2.imwrite(save_path, frame)
-    
-    frame_count += 1
+        # 保存帧为 jpg 图片
+        save_path = os.path.join(output_folder, f"{frame_count:06d}.jpg")
+        cv2.imwrite(save_path, frame)
+        
+        frame_count += 1
+    loop_count += 1
 
 cap.release()
