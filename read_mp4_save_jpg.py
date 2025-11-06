665ca98c8a6bc2f0e2197f9f12c0e79ff4a28c4f
Yu <uchen0223@gmail.com>
2025-11-06 17:27:52 +0800

    random debug trials for interpolation

--- read_mp4_save_jpg.py
+++ read_mp4_save_jpg.py
@@ -14,11 +14,11 @@ cap = cv2.VideoCapture(video_path)
 frame_count, save_count = 0, 0
 while True:
     ret, frame = cap.read()
-    if not ret or save_count >= 100:
+    if not ret:
         break
 
     # 保存帧为 jpg 图片
-    if frame_count % 6 == 0:
+    if frame_count % 3 == 0:
         save_path = os.path.join(output_folder, f"{save_count:06d}.jpg")
         cv2.imwrite(save_path, frame)
         save_count += 1
